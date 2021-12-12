0d220a00c0b3369c3d84c2f903bdc65fe956e2bd
Małgorzata Dudziec <80982768+mdudziec@users.noreply.github.com>
2021-12-12 21:32:30 +0100

    upload, download, code, decode functions

--- src/services/WebSocketService.js
+++ src/services/WebSocketService.js
@@ -1,11 +1,60 @@
 let webSocketsService = {};
 var ws = null;
+var files = [];
+var wsOnmessageListeners = null;
+
+function sendMsgToWs(msg) {
+  ws.send(JSON.stringify(msg));
+  alert("Message is sent private.");
+}
+
+function wsUploadFile(filename, size, bytes) {
+  var msg = {
+    type: "forward",
+    command: "upload",
+    payload: { filepath: filename, path: "", size: size, bytes: bytes },
+  };
+  sendMsgToWs(msg);
+}
+
+function isDownloadable(path) {
+  if (path == "." || path == "..") {
+    return false;
+  }
+  return true;
+}
+function base64ToArrayBuffer(base64) {
+  var binaryString = window.atob(base64);
+  var binaryLen = binaryString.length;
+  var bytes = new Uint8Array(binaryLen);
+  for (var i = 0; i < binaryLen; i++) {
+    var ascii = binaryString.charCodeAt(i);
+    bytes[i] = ascii;
+  }
+  return bytes;
+}
+function saveByteArray(reportName, byte) {
+  let mimeType = "application/octet-stream";
+  // let mimeType = mimeTypes[reportName.lastIndexOf(".")+1]
+  var blob = new Blob([byte], { type: mimeType });
+  var link = document.createElement("a");
+  link.href = window.URL.createObjectURL(blob);
+  var fileName = reportName;
+  link.download = fileName;
+  link.click();
+}
+function onDownloadedFileFromPhone(message) {
+  let decoded_bytes = base64ToArrayBuffer(message.payload.bytes);
+  saveByteArray(message.payload.filename, decoded_bytes);
+}
+
 // var demoEnabled = false;
 
 webSocketsService.install = function (Vue) {
   Vue.config.globalProperties.$webSocketsConnect = function () {
     console.log("Starting Connection to WebSocket Server");
     ws = new WebSocket("wss://seredynski.com:9292/");
+    // todo remove me. why?
     this.ws = ws;
     ws.onopen = () => {
       console.log("Now is connected!");
@@ -39,9 +88,69 @@ webSocketsService.install = function (Vue) {
     ws.send(JSON.stringify(msg));
     alert("Message is sent.");
   };
-  Vue.config.globalProperties.$GetFiles = (obj) => {
+
+  // todo: rename to: parseListFiles()
+  Vue.config.globalProperties.$parseListFiles = function (obj) {
     let fileList = obj.payload;
-    return fileList;
+    files = fileList;
+    if (isDownloadable(files.filename) && files.dir) {
+      var link = document.createElement("a");
+      // link.setAttribute('href', "http://www.ilovethismusic.com");
+      link.setAttribute("target", "_blank");
+    }
+  };
+
+  Vue.config.globalProperties.$wsDownloadFile = function (path) {
+    var msg = { type: "forward", command: "download", path: path };
+    this.webSocketsSend(msg);
+  };
+
+  Vue.config.globalProperties.$GetSavedFiles = function () {
+    return files;
+  };
+  Vue.config.globalProperties.$wsListFiles = function () {
+    var msg = { type: "forward", command: "list-files" };
+    sendMsgToWs(msg);
+  };
+
+  Vue.config.globalProperties.$addWsOnMessageListener = function (
+    listenerFunction
+  ) {
+    wsOnmessageListeners = listenerFunction; //.push(listenerFunction);
+  };
+
+  Vue.config.globalProperties.$dropHandler = (ev) => {
+    console.log("File(s) dropped");
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (ev.dataTransfer.items[i].kind === "file") {
+          var file = ev.dataTransfer.items[i].getAsFile();
+          console.log("... file[" + i + "].name = " + file.name);
+          var reader = new FileReader();
+          reader.readAsArrayBuffer(file);
+          reader.file = file;
+          reader.onloadend = function () {
+            var data = reader.result;
+            var base64data = btoa(
+              String.fromCharCode.apply(null, new Uint8Array(data))
+            );
+            wsUploadFile(this.file.name, this.file.size, base64data);
+            // console.log(base64data);
+          };
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (var j = 0; j < ev.dataTransfer.files.length; j++) {
+        console.log(
+          "... file[" + j + "].name = " + ev.dataTransfer.files[j].name
+        );
+      }
+    }
   };
 
   /*
@@ -50,15 +159,23 @@ webSocketsService.install = function (Vue) {
   function parseMessage(received_msg) {
     let obj = JSON.parse(received_msg);
     if (obj.type == "web-loging-with-code" && obj.result == 0) {
-      wsListFiles();
+      // wsListFiles();
     } else if (obj.command == "list-files") {
       // todo: potential js injection
-      Vue.config.globalProperties.$GetFiles;
+      Vue.config.globalProperties.$parseListFiles(obj);
+      if (wsOnmessageListeners != null) {
+        wsOnmessageListeners(obj);
+      } else if (obj.command == "download") {
+        // todo: potential js injection
+        // file_list = obj.payload
+        onDownloadedFileFromPhone(obj);
+      }
+
+      // for (let index = 0; index < wsOnmessageListeners.length; index++) {
+      //   const listener = wsOnmessageListeners[index];
+      //   listener(obj);
+      // }
     }
-  }
-  function wsListFiles() {
-    var msg = { type: "forward", command: "list-files", demo: 1 };
-    Vue.config.globalProperties.$webSocketsSend(msg);
   }
 };
 
