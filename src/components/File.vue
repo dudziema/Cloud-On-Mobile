58f5ed14cd2b66d41ab5b3cc2027bcb6200d99cb
Małgorzata Dudziec <dudziec.malgorzata@gmail.com>
2022-01-09 14:53:36 +0100

    changed array of serviced size of files

--- src/components/File.vue
+++ src/components/File.vue
@@ -58,7 +58,7 @@ export default {
 
       const k = 1024;
       const dm = decimals < 0 ? 0 : decimals;
-      const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+      const sizes = ["Bytes", "KB", "MB"];
 
       const i = Math.floor(Math.log(bytes) / Math.log(k));
 
