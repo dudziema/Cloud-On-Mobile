168393f7140d74f81a0e4b872c4680a6d4d05961
Małgorzata Dudziec <dudziec.malgorzata@gmail.com>
2022-01-11 19:14:18 +0100

    added responsive design to each breakpoint of width screen

--- src/components/Passcode.vue
+++ src/components/Passcode.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container">
-    <div class="empty-space"></div>
     <div class="enter">Enter the access code</div>
     <div class="instruction">
       To connect with your device, please enter the access code displayed in the
       mobile app.
     </div>
-    <div class="styling"></div>
+
     <div class="passcode-inputs" id="passcode">
       <input
         class="input-first"
@@ -50,9 +49,9 @@
         type="text"
         maxlength="1"
       />
+      <br />
+      <div class="info">Where can I get the access code?</div>
     </div>
-
-    <div class="info">Where can I get the access code?</div>
 
     <div class="button-position">
       <router-link
@@ -69,6 +68,7 @@
     </div>
 
     <div class="stores">
+      <br />
       <Playstore class="google-button" /><img :src="image" /><Appstore
         class="appstore-button"
       /><img :src="image" />
@@ -164,45 +164,414 @@ export default {
 };
 </script>
 <style scoped>
-img {
-  width: 1.5rem;
-  padding-left: 1rem;
-  opacity: 0.5;
-  padding-bottom: 1rem;
+@media screen and (min-width: 0px) and (max-width: 576px) {
+
+  .enter {
+    grid-column: 1 / 12;
+    font-size: 24px;
+    text-align: center;
+    justify-content: center;
+    display: flex;
+    align-self: end;
+    margin-top: 4rem;
+  }
+  .instruction {
+    font-size: 16px;
+    grid-column: 2/ 10;
+    text-align: center;
+  }
+  .passcode-inputs {
+    align-self: start;
+    grid-column: 2/ 12;
+  }
+  .input,
+  .input-first,
+  .input-last {
+    height: 12vw;
+    width: 9vw;
+  }
+  .input {
+    margin: 1vh;
+  }
+  .input-first {
+    margin: 1vh;
+  }
+  .input-last {
+    margin: 1vh;
+  }
+  .info {
+    grid-column: 1 / 12;
+    margin-left: 1.5rem;
+  }
+  .button-position {
+    grid-column: 1 / 12;
+    margin-top: 5vw;
+    justify-content: center;
+    display:flex;
+  }
+  .button {
+    width: 0px;
+  }
+  .disabledButton {
+    width: 118px;
+  }
+  .stores {
+    grid-column: 1/10;
+    align-self: center;
+    align-items: center;
+  }
+  .appstore-button {
+    grid-column: 10/10;
+    width: 20vw;
+    margin-left:10vw;
+  }
+  .google-button {
+    grid-column: 4/5;
+    margin-left: 3rem;
+    width: 20vw;
+  }
+  img {
+    width: 5vw;
+    height: 5vw;
+    padding-left: 1rem;
+    opacity: 0.5;
+    padding-bottom: 1rem;
+  }
 }
+
+@media screen and (min-width: 576px) and (max-width: 768px) {
+  .enter {
+    grid-column: 3 / 9;
+    font-size: 36px;
+    align-self: end;
+
+    text-align: center;
+  }
+  .instruction {
+    align-items: center;
+    grid-column: 3 /9;
+    text-align: center;
+    font-size: 16px;
+  }
+  .passcode-inputs {
+    margin-top: 1vw;
+    grid-column: 3 /12;
+    margin-left: 2vw;
+    width: 600px;
+    align-items: center;
+  }
+  .input,
+  .input-first,
+  .input-last {
+    height: 61px;
+    width: 64px;
+  }
+  .input {
+    margin: 5px;
+  }
+  .input-first {
+    margin: 10px;
+    margin-left: 0vw;
+  }
+  .input-last {
+    margin: 10px;
+    margin-right: 0vw;
+  }
+  .info {
+    grid-column: 3/ 10;
+  }
+  .button-position {
+    grid-column: 3 / 9;
+    align-self: end;
+    margin-top: 3vw;
+    margin-left: 2vw;
+  }
+  .button {
+    width: 444px;
+  }
+  .disabledButton {
+    width: 444px;
+  }
+  .stores {
+    grid-column: 3/9;
+    align-self: end;
+
+    margin-left: 2vw;
+  }
+  .appstore-button {
+    grid-column: 10/10;
+    width: 20vw;
+    margin-left:10vw;
+  }
+  .google-button {
+    grid-column: 4/6;
+    margin-left: 0rem;
+    width: 20vw;
+  }
+  img {
+    width: 5vw;
+    height: 5vw;
+    padding-left: 1rem;
+    opacity: 0.5;
+    padding-bottom: 1rem;
+  }
+}
+@media screen and (min-width: 768px) and (max-width: 992px) {
+  .enter {
+    grid-column: 3 / 9;
+    font-size: 36px;
+    align-self: end;
+
+    text-align: center;
+  }
+  .instruction {
+    align-items: center;
+    grid-column: 3 /9;
+    text-align: center;
+    font-size: 16px;
+  }
+  .passcode-inputs {
+    margin-top: 1vw;
+    grid-column: 3 /12;
+    margin-left: 2vw;
+    width: 600px;
+    align-items: center;
+  }
+  .input,
+  .input-first,
+  .input-last {
+    height: 61px;
+    width: 64px;
+  }
+  .input {
+    margin: 10px;
+  }
+  .input-first {
+    margin: 10px;
+    margin-left: 0vw;
+  }
+  .input-last {
+    margin: 10px;
+    margin-right: 0vw;
+  }
+  .info {
+    grid-column: 3/ 10;
+  }
+  .button-position {
+    grid-column: 3 / 9;
+    align-self: end;
+    margin-top: 3vw;
+    margin-left: 2vw;
+  }
+  .button {
+    width: 500px;
+  }
+  .disabledButton {
+    width: 500px;
+  }
+ .stores {
+    grid-column: 3/9;
+    align-self: end;
+
+    margin-left: 2vw;
+  }
+  .appstore-button {
+    grid-column: 6/10;
+    width: 10vw;
+    margin-left:3vw;
+  }
+  .google-button {
+    grid-column: 4/6;
+    margin-left: 0rem;
+    width: 10vw;
+  }
+  img {
+    width: 5vw;
+    height: 5vw;
+    padding-left: 1rem;
+    opacity: 0.5;
+    padding-bottom: 1rem;
+  }
+}
+@media screen and (min-width: 992px) and (max-width: 1200px) {
+  .enter {
+    grid-column: 4 / 9;
+    font-size: 36px;
+    align-self: end;
+
+    text-align: center;
+  }
+  .instruction {
+    align-items: center;
+    grid-column: 4 /9;
+    text-align: center;
+    font-size: 16px;
+  }
+  .passcode-inputs {
+    margin-top: 1vw;
+    grid-column: 4 /12;
+    margin-left: 2vw;
+    width: 600px;
+    align-items: center;
+  }
+  .input,
+  .input-first,
+  .input-last {
+    height: 61px;
+    width: 64px;
+  }
+  .input {
+    margin: 10px;
+  }
+  .input-first {
+    margin: 10px;
+    margin-left: 0vw;
+  }
+  .input-last {
+    margin: 10px;
+    margin-right: 0vw;
+  }
+  .info {
+    grid-column: 4 / 10;
+  }
+  .button-position {
+    grid-column: 4 / 9;
+    align-self: end;
+    margin-top: 3vw;
+    margin-left: 2vw;
+  }
+  .button {
+    width: 500px;
+  }
+  .disabledButton {
+    width: 500px;
+  }
+  .stores {
+    grid-column: 4/9;
+    align-self: end;
+
+    margin-left: 2vw;
+  }
+  .appstore-button {
+    grid-column: 10/10;
+    width: 11vw;
+    margin-left:2vw;
+  }
+  .google-button {
+    grid-column: 4/5;
+    margin-left: 3rem;
+    width: 11vw;
+  }
+  img {
+    width: 5vw;
+    height: 5vw;
+    padding-left: 1rem;
+    opacity: 0.5;
+    padding-bottom: 1rem;
+  }
+}
+@media screen and (min-width: 1200px) {
+  .enter {
+    grid-column: 3 / 6;
+    font-size: 230%;
+    align-self: end;
+    margin-top: 6vw;
+  }
+  .instruction {
+    font-size: 1rem;
+    grid-column: 3 / 9;
+    width: 445px;
+  }
+  .passcode-inputs {
+    align-self: start;
+    grid-column: 3 / 12;
+    width: 445px;
+  }
+  .input,
+  .input-first,
+  .input-last {
+    height: 61px;
+    width: 64px;
+  }
+  .input {
+    margin: 5px;
+  }
+  .input-first {
+    margin-right: 5px;
+    margin-left: 0vw;
+  }
+  .input-last {
+    margin-left: 5px;
+    margin-right: 0vw;
+  }
+  .info {
+    grid-column: 3 / 7;
+  }
+  .button-position {
+    grid-column: 3 / 12;
+    margin-top: 10px;
+  }
+  .button {
+    width: 435px;
+  }
+  .disabledButton {
+    width: 435px;
+  }
+ .stores {
+    grid-column: 3/9;
+    align-self: end;
+
+    margin-left: 2vw;
+  }
+  .appstore-button {
+    grid-column: 6/10;
+    width: 7vw;
+    margin-left:3vw;
+  }
+  .google-button {
+    grid-column: 4/6;
+    margin-left: 0rem;
+    width: 7vw;
+  }
+  img {
+    width: 2vw;
+    height: 2vw;
+    padding-left: 1rem;
+    opacity: 0.5;
+    padding-bottom: 1rem;
+  }
+}
+
 .container {
   display: grid;
-  grid-template: repeat(8, 1fr) / repeat(12, 1fr);
+  grid-template: repeat(5, 1fr) / repeat(10, 1fr);
   grid-gap: 3px;
 }
 
 .enter {
   /* Enter the access code */
-  grid-column: 3 / 6;
-  margin-right: auto;
   white-space: nowrap;
-  align-self: end;
 
   /* Heading/Web */
   font-family: "SF-Pro-Text";
   font-style: normal;
   font-weight: 600;
-  font-size: 230%;
+
   line-height: 3rem;
-  text-align: center;
+
   letter-spacing: -0.019rem;
   color: #0c0c0c;
 }
 
 .instruction {
   /* To connect with your device, please enter the access code displayed in the mobile app. */
-  grid-column: 3 / 9;
+
   margin-right: auto;
   /* Body/16/Regular */
   font-family: Poppins;
   font-style: normal;
   font-weight: normal;
-  font-size: 1rem;
+
   line-height: 1.5rem;
   letter-spacing: 0.003rem;
   color: #0c0c0c;
@@ -210,11 +579,8 @@ img {
 }
 
 .passcode-inputs {
-  align-self: end;
-  grid-column: 3 / 12;
-
   white-space: nowrap;
-  overflow: hidden;
+
   text-overflow: ellipsis;
 }
 input:focus {
@@ -235,8 +601,6 @@ input:hover {
 .input,
 .input-first,
 .input-last {
-  height: 3.25rem;
-  width: 2.793378rem;
   text-align: center;
   font-family: Poppins;
   font-weight: 900;
@@ -250,20 +614,9 @@ input:hover {
   border-style: none;
 }
 
-.input {
-  margin: 2%;
-}
-.input-first {
-  margin-right: 2%;
-  margin-left: 0%;
-}
-.input-last {
-  margin-left: 2%;
-  margin-right: 0%;
-}
 .info {
   /* Where can I get the access code? */
-  grid-column: 3 / 7;
+
   align-self: start right;
   white-space: nowrap;
   overflow: hidden;
@@ -297,14 +650,13 @@ input:hover {
   grid-column: 4/5;
 }
 .button-position {
-  grid-column: 3 / 9;
   align-self: start;
 }
 .button {
   /* button */
 
   /* Auto Layout */
-  width: 400px;
+
   height: 56px;
   white-space: nowrap;
   overflow: hidden;
@@ -320,7 +672,6 @@ input:hover {
   background: #0e70f1;
 }
 .disabledButton {
-  width: 400px;
   height: 56px;
   white-space: nowrap;
   overflow: hidden;
@@ -341,6 +692,7 @@ input:hover {
 
   width: 4.313rem;
   height: 1.5rem;
+  white-space: nowrap;
 
   /* Button */
 
@@ -364,28 +716,26 @@ input:hover {
   order: 0;
   flex-grow: 0;
 }
+.download {
+  /* Body/16/Regular */
 
-.stores {
-  grid-column: 3/12;
-  align-self: end;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 24px;
+  /* identical to box height, or 150% */
+  letter-spacing: 0.003em;
+  width: 199px;
+  color: #000000;
+
+  opacity: 0.8;
 }
+
 .appstore-button {
-  grid-column: 10/10;
   align-self: end;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  padding-left: 5rem;
-  width: 95px;
 }
 .google-button {
-  grid-column: 4/5;
   align-self: end;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  padding-left: 2rem;
-
-  width: 100px;
 }
 </style>
